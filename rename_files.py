52892ba9b374a8469bb0969248a81b2691da13ab
Analin Flores <foglzerika@gmail.com>
2017-09-17 18:38:27 +0200

    Function file list fished

--- rename_files.py
+++ rename_files.py
@@ -2,6 +2,7 @@ import os
 #The reserved word=> def is for defining a function followed of  the name of funcition
 def rename_files() :
     #(1) get files names from a folder
-    file_list = os.listdir(r"C:\Users\Anys\Documents\python\secretMessage")
-    print (file_list
+    file_list = os.listdir(r"C:\Users\Anys\Documents\python\secretMessage\prank")
+    print (file_list)
     #(2)for each file, rename filename
+rename_files()
