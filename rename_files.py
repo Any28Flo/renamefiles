f426fcfd1c793c8a26c7f5bbfa463dd0ffe2a749
Analin Flores <foglzerika@gmail.com>
2017-09-18 07:56:51 -0500

    Create function rename files

--- rename_files.py
+++ rename_files.py
@@ -5,4 +5,7 @@ def rename_files() :
     file_list = os.listdir(r"C:\Users\Anys\Documents\python\secretMessage\prank")
     print (file_list)
     #(2)for each file, rename filename
+    for filename in file_list :
+        os.rename(filename)
+        os.rename(filename, filename.translate(None, "0123456789"))
 rename_files()
